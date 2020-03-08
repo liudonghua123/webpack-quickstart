b54929985508c6af8e49cfdd230dab731f2c6644
liudonghua123 <liudonghua123@gmail.com>
2020-03-08 15:59:23 +0800

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const config = require('./public/config')[isDev ? 'dev' : 'build'];
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
+  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
       {
@@ -20,4 +21,13 @@ module.exports = {
       config: config.template,
     }),
   ],
+  devServer: {
+    port: '3000', //默认是8080
+    quiet: false, //默认不启用
+    inline: true, //默认开启 inline 模式，如果设置为false,开启 iframe 模式
+    stats: 'errors-only', //终端仅打印 error
+    overlay: true, //默认不启用
+    clientLogLevel: 'silent', //日志等级
+    compress: true, //是否启用 gzip 压缩
+  },
 };
